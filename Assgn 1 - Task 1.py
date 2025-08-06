eef678b64e4989cbcde66527b0f69e7783a4a4f8
VaishnavSanampalli <vaishnavsanampalli@gmail.com>
2025-08-06 09:53:22 +0530

    Add files via upload

---
+++ Assgn 1 - Task 1.py
@@ -0,0 +1,10 @@
+print ("hello world")
+A = float(input("Enter a number = "))
+B = float(input("Enter another number = "))
+
+print(f"Addition = {A + B}")
+print(f"Subtraction = {A - B}")
+print(f"Multiplication = {A * B}")
+print(f"Division = {A / B}")
+
+print("Thank You")
